d2f5d3fec5686215b12b7fc24457a53c0d5feac3
Martin <garbade.work@gmail.com>
2025-02-12 16:25:37 +0100

    [fix] dockerControl - docker started and stopped status

--- src/renderer/dockerControl.js
+++ src/renderer/dockerControl.js
@@ -23,14 +23,17 @@ document.addEventListener('DOMContentLoaded', () => {
     
     window.electronAPI.onContainersStarted((event, message) => {
         console.log('Containers started successfully');
+        updateStatus('Containers started.')
     });
-
+    
     window.electronAPI.onContainersStopped((event, message) => {
         console.log('Containers stopped successfully');
+        updateStatus('Containers stopped.')
     });
-
+    
     window.electronAPI.onSoftwareRemoved((event, message) => {
         console.log('Software removed successfully');
+        updateStatus('Containers removed.')
     });
 
     window.electronAPI.onDockerLog((event, message) => {
